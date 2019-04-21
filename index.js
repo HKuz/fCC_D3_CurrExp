740d98e669b3ebff9ec112a471a2fd5666d1261a
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-20 22:42:21 -0400

    fix: add console statements to debug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("Starting script");
 // GeoJSON data to draw country outlines
 const mapURL = "https://d3js.org/world-110m.v1.json";
 
@@ -12,12 +13,12 @@ let svg = d3.select("#map")
             .append("svg")
             .attr("width", width)
             .attr("height", height);
-
+console.log("SVG appended");
 let projection = d3.geoMercator()
                    .scale(scale)
                    .translate([width / 2, height / 2]);
 
-let path = d3.geoPath(projection);
+let path = d3.geoPath();
 
 d3.json(mapURL, function(json) {
         console.log("In JSON call")
