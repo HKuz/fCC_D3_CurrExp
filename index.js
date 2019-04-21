d18dcbc5cab7cd2351862439e151828592a85c45
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-21 07:36:10 -0400

    feat: add topojson call to access JSON map features

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("Starting script");
 // GeoJSON data to draw country outlines
 const mapURL = "https://d3js.org/world-110m.v1.json";
 
@@ -7,23 +6,24 @@ const mapURL = "https://d3js.org/world-110m.v1.json";
 // Setup
 const width = 960;
 const height = 500;
-const scale = 250;
+const scale = 130;
 
 let svg = d3.select("#map")
             .append("svg")
             .attr("width", width)
             .attr("height", height);
-console.log("SVG appended");
+
 let projection = d3.geoMercator()
                    .scale(scale)
                    .translate([width / 2, height / 2]);
 
-let path = d3.geoPath();
+let path = d3.geoPath().projection(projection);
 
-d3.json(mapURL, function(json) {
-        console.log("In JSON call")
+d3.json(mapURL).then(function(json) {
+        const countries = topojson.feature(json, json.objects.countries)
+                                  .features;
         svg.selectAll("path")
-           .data(json.features)
+           .data(countries)
            .enter()
            .append("path")
            .attr("d", path)
