2187189626688e54c37b464ee5eab544650bb905
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-24 17:50:12 -0400

    fix: update let keyword with const where needed

--- index.js
+++ index.js
@@ -5,29 +5,39 @@ const mapURL = "https://d3js.org/world-110m.v1.json";
 // World population source: https://databank.worldbank.org
 
 // Setup
+const format = d3.format(",");
 const width = 1000;
 const height = 650;
 const scale = width / (2 * Math.PI);
 
-let svg = d3.select("#map")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
+const svg = d3.select("#map")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height);
 
-let projection = d3.geoMercator()
-                   .scale(scale)
-                   .translate([width / 2, height / 2]);
+const projection = d3.geoMercator()
+                     .scale(scale)
+                     .translate([width / 2, height / 2]);
 
-let path = d3.geoPath()
-             .projection(projection);
+const path = d3.geoPath()
+               .projection(projection);
 
 d3.json(mapURL).then(function(json) {
-        const countries = topojson.feature(json, json.objects.countries)
-                                  .features;
-        svg.selectAll("path")
-           .data(countries)
-           .enter().append("path")
-            .attr("d", path)
-            .style("fill", "gray")
-            .style("stroke", "white");
-});
+  const countries = topojson.feature(json, json.objects.countries)
+                            .features;
+
+  svg.append("g")
+     .attr("class", "countries")
+     .selectAll("path")
+     .data(countries)
+     .enter().append("path")
+       .attr("d", path)
+       .style("fill", "gray")
+       .style("stroke", "white");
+
+  // svg.append('path')
+  //   .datum(topojson.mesh(data.features, (a, b) => a.id !== b.id))
+  //   .attr('class', 'names')
+  //   .attr('d', path);
+
+}); // end d3.json promise
