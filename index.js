a80dbd70ba6208d1be5e0c71bc971872bc53c0cb
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-26 17:02:05 -0400

    fix: adjust indenting for consistency

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const height = 700;
 const scale = width / (2 * Math.PI);
 
 const svg = d3.select("#map")
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height);
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 const projection = d3.geoMercator()
-                     .scale(scale)
-                     .translate([width / 2, height / 2])
-                     .center([0, 20]);
+    .scale(scale)
+    .translate([width / 2, height / 2])
+    .center([0, 20]);
 
 const path = d3.geoPath()
-               .projection(projection);
+    .projection(projection);
 
 // Create a group to hold the countries
 const g = svg.append("g");
@@ -43,15 +43,15 @@ Promise.all([getCSVData, getJSONData]).then(function(values) {
 
   // Create a scale to map population value to a color
   const color = d3.scaleThreshold()
-                  .domain([
-                    500000,
-                    5000000,
-                    10000000,
-                    25000000,
-                    50000000,
-                    100000000,
-                    1000000000])
-                  .range(d3.schemeYlOrRd[8]);
+      .domain([
+        500000,
+        5000000,
+        10000000,
+        25000000,
+        50000000,
+        100000000,
+        1000000000])
+      .range(d3.schemeYlOrRd[8]);
 
   // Create an object that maps country name to population
   let popMap = {};
@@ -59,71 +59,70 @@ Promise.all([getCSVData, getJSONData]).then(function(values) {
     popMap[d.Name] = +d.Population
   });
 
-  const countries = topojson.feature(json, json.objects.countries)
-                            .features;
+  const countries = topojson.feature(json, json.objects.countries).features;
 
   // Set up tooltip
   const tooltip = d3.select("#tooltip")
-                    .classed("tooltip", true);
+      .classed("tooltip", true);
 
   // Draw the map and add tooltip functionality
   g.selectAll(".countries")
-     .data(countries)
-   .enter().append("path")
-     .attr("class", "countries")
-     .attr("d", path)
-     .style("stroke", "white")
-     .style("stroke-width", 0.5)
-     .style("fill", d => {
-       const pop = popMap[d.properties.name];
-       if (pop) {
-         return color(pop);
-       } else {
-         return "gray"
-       }
-     })
-     .style("opacity", 0.75)
-     .on("mouseover", function(d) {
-       const pop = popMap[d.properties.name] ? format(popMap[d.properties.name]) : "NA";
+    .data(countries)
+    .enter().append("path")
+      .attr("class", "countries")
+      .attr("d", path)
+      .style("stroke", "white")
+      .style("stroke-width", 0.5)
+      .style("fill", d => {
+        const pop = popMap[d.properties.name];
+        if (pop) {
+          return color(pop);
+        } else {
+          return "gray"
+        }
+      })
+      .style("opacity", 0.75)
+      .on("mouseover", function(d) {
+        const pop = popMap[d.properties.name] ? format(popMap[d.properties.name]) : "NA";
 
-       // Create HTML string with country name and population info
-       let dataPoint = "<div>" +
-                       "<strong><span class='label'>Country: </span></strong>" +
-                       d.properties.name + "<br />" +
-                       "<strong><span class='label'>Population: </span></strong>" +
-                       pop +
-                       "</div>";
+        // Create HTML string with country name and population info
+        let dataPoint = "<div>" +
+            "<strong><span class='label'>Country: </span></strong>" +
+            d.properties.name + "<br />" +
+            "<strong><span class='label'>Population: </span></strong>" +
+            pop +
+            "</div>";
 
-       tooltip.transition()
-         .style("opacity", .9);
+        tooltip.transition()
+          .style("opacity", .9);
 
-       tooltip.html(dataPoint)
-         .style("left", (d3.event.pageX + 5) + "px")
-         .style("top", (d3.event.pageY - 28) + "px");
+        tooltip.html(dataPoint)
+          .style("left", (d3.event.pageX + 5) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
 
-       // Change the style of the selected country
-       d3.select(this)
-         .style("opacity", 1)
-         .style("stroke-width", 2);
-     })
-     .on("mouseout", function(d) {
-       // Fade tooltip when mouse leaves
-       tooltip.transition()
-         .style("opacity", 0);
+        // Change the style of the selected country
+        d3.select(this)
+          .style("opacity", 1)
+          .style("stroke-width", 2);
+      })
+      .on("mouseout", function(d) {
+        // Fade tooltip when mouse leaves
+        tooltip.transition()
+          .style("opacity", 0);
 
-       // Revert country to original style
-       d3.select(this)
-         .style("opacity", 0.75)
-         .style("stroke-width", 0.5);
-     });
+        // Revert country to original style
+        d3.select(this)
+          .style("opacity", 0.75)
+          .style("stroke-width", 0.5);
+      });
 
   // Add map pan and zoom behavior
   const pad = 140;
 
   svg.call(d3.zoom()
-    .scaleExtent([1, 8])
-    .translateExtent([[0, -pad], [width, height + pad]])
-    .on("zoom", zoomed)
+      .scaleExtent([1, 8])
+      .translateExtent([[0, -pad], [width, height + pad]])
+      .on("zoom", zoomed)
   );
 
   function zoomed() {
