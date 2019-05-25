80221229d242e65f89a2769ce268d0c7dafa50e1
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-25 15:24:02 -0400

    feat: start lesson 13

--- Lessons/0130_index.js
+++ Lessons/0130_index.js
@@ -1,3 +1,16 @@
+
+
+/*
+With the project files set up, it's time to start laying out the map. First, you
+need the data that will describe the shape of the countries. This coordinate
+information, along with various metadata, is saved in a specific form of `JSON`
+- either geoJSON or topoJSON.
+
+We've saved a topoJSON file named `naturalEarth50TopoJSON.json` in the project
+folder. Using the `const` keyword, create a variable named `mapPath` set to a
+string of the file path.
+*/
+
 // Setup
 const width = 1000;
 const height = 700;
@@ -6,8 +19,3 @@ const svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
-
-
-/*
-
-*/
