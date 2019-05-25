626dd8074c41be6dc9eac6f738b13a3870fcd131
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-25 11:52:37 -0400

    feat: create lesson 9

---
+++ Lessons/0090_index.js
@@ -0,0 +1,15 @@
+// Setup
+const width = 1000;
+const height = 700;
+
+const svg = d3.select("#map")
+
+
+/*
+D3's methods support chaining, which means you can link several together in a row
+to accomplish what you're trying to do. The next step your script should tack an
+`svg` element to the `div` you have selected in your `svg` variable.
+
+Chain D3's `append()` method to the end of the `select()` one, and pass it the
+argument `"svg"`.
+*/
