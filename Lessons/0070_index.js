643c690ecc3ef47497cada3bcaa9eaa363016513
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-25 09:24:23 -0400

    feat: create lesson 7

---
+++ Lessons/0070_index.js
@@ -0,0 +1,14 @@
+// Setup
+
+
+/*
+As noted in the first lesson, D3 is designed to work with common web standards,
+such as HTML, CSS, and Scalable Vector Graphics (SVG). This project will use D3
+to draw a map with SVG, which you'll add to your page with this script.
+
+SVG elements have `height` and `width` attributes, so the browser knows the size
+and how to display them.
+
+Using the `const` keyword, declare a `width` variable and set it to `1000`, and
+declare a `height` variable and set it to `700`.
+*/
