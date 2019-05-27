dd9b4965c19df698774dc7bce6897dafc28cc96b
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-27 18:31:36 -0400

    feat: start lesson 14 svg positioning

--- Lessons/0140_index.js
+++ Lessons/0140_index.js
@@ -1,24 +1,3 @@
-// topoJSON data to draw country outlines
-// Source: Natural Earth 1:50m Cultural Vectors, Admin 0 - Countries
-const mapPath = "./naturalEarth50TopoJSON.json";
-
-// 2017 world population data. Source: https://databank.worldbank.org
-
-
-
-/*
-As noted in the comment, the map data source is Natural Earth. The datasets are
-available to use in the public domain.
-
-The "50" in the TopoJSON name is a reference to the level of detail (1:50m scale)
-and is the middle option. It's a compromise between having enough country
-information while keeping the file size reasonable.
-
-The population data you'll show in the map is a CSV file we downloaded from the
-World Bank website. Using the `const` keyword, create a variable named `popPath`,
-and set it to the path to a file named `worldPopulation.csv` in the project folder.
-*/
-
 // Setup
 const width = 1000;
 const height = 700;
@@ -27,3 +6,19 @@ const svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
+
+svg.append("rect")
+
+
+
+/*
+For the new rectangle element to appear on the page properly, you need to fill
+in a few more attributes. First off, you have to specify where SVG should start
+drawing the rectangle.
+
+SVG uses a basic `x, y` coordinate system to position elements, and the origin is
+in the top-left corner. This means higher `x` values push an element farther to
+the right, and higher `y` values push it farther down.
+
+
+*/
