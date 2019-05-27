0f188760ef4b167b1057a577454bd9fc7de2c6b9
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-27 18:21:10 -0400

    feat: add first SVG lesson

--- Lessons/0130_index.js
+++ Lessons/0130_index.js
@@ -1,27 +1,3 @@
-// topoJSON data to draw country outlines
-// Source: Natural Earth 1:50m Cultural Vectors, Admin 0 - Countries
-
-
-
-/*
-The first step to creating a map is getting the data that will describe the shape
-of the countries. This information, along with various metadata, is saved in a
-specific form of `JSON` - either GeoJSON or TopoJSON.
-
-GeoJSON is a data structure that holds geographic features. They can include
-points (such as a city), lines (a street), or complicated multipolygons (a
-boundary) - along with their coordinates.
-
-TopoJSON is an extension of GeoJSON, but it gets rid of duplication. A shared
-boundary between two countries would be saved only once as an arc. This results
-in smaller files that are easier to use in a web application.
-
-There's a TopoJSON file named `naturalEarth50TopoJSON.json` in the project
-folder. Using the `const` keyword, create a variable named `mapPath` that holds
-the path to the file. You'll use this variable to tell D3 where to find the data
-to draw the country outlines for your map.
-*/
-
 // Setup
 const width = 1000;
 const height = 700;
@@ -30,3 +6,18 @@ const svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
+
+
+
+/*
+We're going to take a quick detour from setting up the map to explain a few
+concepts about SVG. SVG is a web standard for 2-dimensional images, but also
+supports interaction and animation.
+
+SVG supports a variety of different shapes. With D3, you can add shapes to your
+`svg` element with the `.append()` method, passing it the name of the shape as a
+string. For example, you'd add a circle by writing `svg.append("circle")`.
+
+Using the `circle` example above as a template, add a rectangle (`"rect"`)
+element to the page.
+*/
