aa127e0521c1a2703cdc01c43dcb9e006b75a6a6
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-26 20:06:36 -0400

    feat: create lesson 13

--- Lessons/0130_index.js
+++ Lessons/0130_index.js
@@ -2,13 +2,22 @@
 
 /*
 With the project files set up, it's time to start laying out the map. First, you
-need the data that will describe the shape of the countries. This coordinate
-information, along with various metadata, is saved in a specific form of `JSON`
-- either geoJSON or topoJSON.
+need the data that will describe the shape of the countries. This information,
+along with various metadata, is saved in a specific form of `JSON` - either
+GeoJSON or TopoJSON.
 
-We've saved a topoJSON file named `naturalEarth50TopoJSON.json` in the project
-folder. Using the `const` keyword, create a variable named `mapPath` set to a
-string of the file path.
+GeoJSON is a data structure that holds geographic features - these can be points
+(a city), lines (a street), or complicated multipolygons (a boundary) - along
+with their coordinates.
+
+TopoJSON is an extension of GeoJSON, but it gets rid of duplication. A shared
+boundary between two countries would be saved only once as an arc. This results
+in smaller files that are easier to use in a web application.
+
+There's a TopoJSON file named `naturalEarth50TopoJSON.json` in the project
+folder. Using the `const` keyword, create a variable named `mapPath` that holds
+the path to the file. You'll use this variable to tell D3 where to find the data
+to draw the country outlines for your map.
 */
 
 // Setup
