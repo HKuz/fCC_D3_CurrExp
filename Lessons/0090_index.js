9c03a6301865afb1aae4bf77a27a479132ade74f
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-28 13:35:27 -0400

    fix: update svg height and width variable names

--- Lessons/0090_index.js
+++ Lessons/0090_index.js
@@ -1,15 +1,16 @@
 // Setup
-const width = 1000;
-const height = 700;
+const svgWidth = 1000;
+const svgHeight = 700;
 
 const svg = d3.select("#map")
 
 
 /*
-D3's methods support chaining, which means you can link several together in a row
-to accomplish what you're trying to do. The next step your script should tack an
-`svg` element to the `div` you have selected in your `svg` variable.
+The next step your script should append an `svg` element to the `div` you have
+selected in your `svg` variable. Fortunately, D3 has an `append()` method that
+does just that.
 
-Chain D3's `append()` method to the end of the `select()` one, and pass it the
-argument `"svg"`.
+D3's methods support chaining, which means you can link several together in a
+row to accomplish what you're trying to do. Chain D3's `append()` method to the
+end of the `select()` one, and pass it the argument `"svg"`.
 */
