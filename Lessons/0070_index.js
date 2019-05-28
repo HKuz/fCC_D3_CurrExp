d5b26aa4f9017e0217362bcf9b146bb967c3336d
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-28 12:31:37 -0400

    fix: update svg height and width variable names

--- Lessons/0070_index.js
+++ Lessons/0070_index.js
@@ -9,6 +9,6 @@ to draw a map with SVG, which you'll add to your page with this script.
 SVG elements have `height` and `width` attributes, so the browser knows the size
 and how to display them.
 
-Using the `const` keyword, declare a `width` variable and set it to `1000`, and
-declare a `height` variable and set it to `700`.
+Using the `const` keyword, declare an `svgWidth` variable and set it to `1000`,
+and declare an `svgHeight` variable and set it to `700`.
 */
