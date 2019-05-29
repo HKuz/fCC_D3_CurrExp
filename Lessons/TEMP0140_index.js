5e02dc056cbd005f20f43e6ab1792c3db8d71fbf
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-29 11:01:31 -0400

    feat: update lesson adding popPath

--- Lessons/TEMP0140_index.js
+++ Lessons/TEMP0140_index.js
@@ -1,4 +1,4 @@
-// topoJSON data to draw country outlines
+// TopoJSON data to draw country outlines
 // Source: Natural Earth 1:50m Cultural Vectors, Admin 0 - Countries
 const mapPath = "./naturalEarth50TopoJSON.json";
 
@@ -7,23 +7,27 @@ const mapPath = "./naturalEarth50TopoJSON.json";
 
 
 /*
-As noted in the comment, the map data source is Natural Earth. The datasets are
-available to use in the public domain.
+As noted in the second comment, the map data source is Natural Earth. The datasets
+are available to use in the public domain.
 
 The "50" in the TopoJSON name is a reference to the level of detail (1:50m scale)
 and is the middle option. It's a compromise between having enough country
-information while keeping the file size reasonable.
+information (it doesn't cut out smaller Caribbean islands) while keeping the file
+size reasonable.
 
-The population data you'll show in the map is a CSV file we downloaded from the
-World Bank website. Using the `const` keyword, create a variable named `popPath`,
-and set it to the path to a file named `worldPopulation.csv` in the project folder.
+The population numbers you'll show in the map are 2017 data in a comma-separated
+value (CSV) file we downloaded from the World Bank website. This file is in the
+project folder under the name `worldPopulation.csv`.
+
+Using the `const` keyword, create a variable named `popPath`, and set it to the
+path for this population data file.
 */
 
 // Setup
-const width = 1000;
-const height = 700;
+const svgWidth = 1000;
+const svgHeight = 700;
 
 const svg = d3.select("#map")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", svgWidth)
+    .attr("height", wvgHeight);
