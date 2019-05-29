ba7cfe84265168051544b4c3207824250b33828e
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-05-29 10:53:57 -0400

    feat: update lesson adding mapPath

--- Lessons/TEMP0130_index.js
+++ Lessons/TEMP0130_index.js
@@ -1,4 +1,4 @@
-// topoJSON data to draw country outlines
+// TopoJSON data to draw country outlines
 // Source: Natural Earth 1:50m Cultural Vectors, Admin 0 - Countries
 
 
@@ -10,23 +10,24 @@ specific form of `JSON` - either GeoJSON or TopoJSON.
 
 GeoJSON is a data structure that holds geographic features. They can include
 points (such as a city), lines (a street), or complicated multipolygons (a
-boundary) - along with their coordinates.
+boundary) - including that feature's coordinates. You'll use the sequence of
+coordinates that describe each country's boundary to draw the `path` elements on
+your `svg`.
 
-TopoJSON is an extension of GeoJSON, but it gets rid of duplication. A shared
-boundary between two countries would be saved only once as an arc. This results
-in smaller files that are easier to use in a web application.
+TopoJSON is an extension of GeoJSON, but it removes duplication. Any shared
+boundaries between countries would be saved only once as an arc. This results in
+smaller files that are easier to use in a web application.
 
 There's a TopoJSON file named `naturalEarth50TopoJSON.json` in the project
 folder. Using the `const` keyword, create a variable named `mapPath` that holds
-the path to the file. You'll use this variable to tell D3 where to find the data
-to draw the country outlines for your map.
+the path to the file.
 */
 
 // Setup
-const width = 1000;
-const height = 700;
+const svgWidth = 1000;
+const svgHeight = 700;
 
 const svg = d3.select("#map")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
