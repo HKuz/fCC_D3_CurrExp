e651bf6474ba0e4314b551e5bfc2501ec3ccd646
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-06-07 18:15:15 -0400

    feat: fix numbering, adj flow from new lessons

--- Lessons/0250_index.js
+++ Lessons/0250_index.js
@@ -4,15 +4,21 @@
 
 
 /*
-The first step to creating a map is getting the data that will describe the shape
-of the countries. This information, along with various metadata, is saved in a
-specific form of `JSON` - either GeoJSON or TopoJSON.
+Your `svg` shapes were lovely, but it's time to get back to drawing the map of
+the world.
+
+The first step is getting the data that will describe the shape of the countries.
+This information, along with various metadata, is saved in a specific form of
+`JSON` - either GeoJSON or TopoJSON.
 
 GeoJSON is a data structure that holds geographic features. They can include
-points (such as a city), lines (a street), or complicated multipolygons (a
-boundary) - including that feature's coordinates. You'll use the sequence of
-coordinates that describe each country's boundary to draw the `path` elements on
-your `svg`.
+points (such as a city), lines (a street), and multipolygons (a boundary) -
+including that feature's coordinates. You'll use the sequence of coordinates
+that describe each country's boundary to draw the `path` elements on your `svg`.
+
+(Don't worry, you won't need to manually create these `paths` like you did in
+the prior lessons- D3 has all the built-in functionality to extract the data and
+generate them for you!)
 
 TopoJSON is an extension of GeoJSON, but it removes duplication. Any shared
 boundaries between countries would be saved only once as an arc. This results in
