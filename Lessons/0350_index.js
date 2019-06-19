bf32396ed37116064371c4451a7f72cac9d9a298
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-06-18 23:15:15 -0400

    feat: setup next lesson

---
+++ Lessons/0350_index.js
@@ -0,0 +1,30 @@
+// topoJSON data to draw country outlines
+// Source: Natural Earth 1:50m Cultural Vectors, Admin 0 - Countries
+const mapPath = "./naturalEarth50TopoJSON.json";
+
+// 2017 world population data. Source: https://databank.worldbank.org
+const popPath = "./worldPopulation.csv";
+
+// Setup
+const svgWidth = 1000;
+const svgHeight = 700;
+const scale = svgWidth / (2 * Math.PI);
+
+const svg = d3.select("#map")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+const projection = d3.geoMercator()
+    .scale(scale)
+    .translate([svgWidth / 2, svgHeight / 2])
+    .center([0, 20]);
+
+const path = d3.geoPath()
+    .projection(projection);
+
+
+
+/*
+[GROUP TO HOLD COUNTRIES]
+*/
