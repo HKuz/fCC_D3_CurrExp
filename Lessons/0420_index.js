bf4e8b628008228c00516a3ab87b4ae4a7c984af
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-06-30 12:51:40 -0400

    feat: create lesson 42 population array with numbers

--- Lessons/0420_index.js
+++ Lessons/0420_index.js
@@ -40,5 +40,23 @@ Promise.all([getJSONData, getCSVData]).then(function(values) {
 
 })
 /*
+Often with data visualizations, you'll want to size or color items based on the
+underlying values in the dataset. For example, in a bar chart, you want the height
+of each bar to tie to the value it's showing. In your map, you're going to color
+the country based on how many people live there.
 
+D3 scales provide the functionality to do this. But before getting into scales,
+you're going to want to understand your data. The data type (discrete or
+continuous), how they're distributed, and what you're trying to visualize will
+all help to determine the type of scale that is most appropriate to use.
+
+When D3 imported the CSV data (available in `population.Population`), it loaded
+the figures as strings. You need to first map them into an array as numbers before
+you can analyze them.
+
+Using the `const` keyword, declare a variable named `popArray`, then apply the
+`.map()` method on `population`. Pass it a callback function with one parameter
+`d` that returns the population figures changed to numbers (you can access them
+with `d.Population`). A shortcut to cast a string to a number in JavaScript is to
+add a `+` in front of the string, for example `+numAsString`.
 */
